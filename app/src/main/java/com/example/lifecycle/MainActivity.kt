892957c84608492e53cdb7391cc56d90c3ca8984
Administrator <godxuche@gmail.com>
2021-10-29 15:52:38 +0100

    observing the liveData variable to update the UI...

--- app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lifecycle
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.lifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val loginViewModel by lazy { ViewModelProvider(this)[LoginViewModel::class.java] }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,6 +21,21 @@ class MainActivity : AppCompatActivity() {
         val txtUsername = binding.txtUsername
         val txtPassword = binding.txtPassword
         val btnLogin = binding.btnLogin
+        val txtStatus = binding.txtStatus
 
+        btnLogin.setOnClickListener {
+            loginViewModel.login(txtUsername.text.toString(), txtPassword.text.toString())
+        }
+
+        //observing the liveData
+        loginViewModel.loginStatus.observe(this, Observer {
+            if (it) {
+                txtStatus.setTextColor(Color.rgb(0, 128, 0))
+                txtStatus.text = "Logged in"
+            } else {
+                txtStatus.setTextColor(Color.RED)
+                txtStatus.text = "Not logged in"
+            }
+        })
     }
 }
